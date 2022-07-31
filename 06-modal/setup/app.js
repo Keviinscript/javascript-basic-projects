194ae56eb823fe04034a6f84dd4244f9682ac073
ikechukwu kevin-chika <101973303+Keviinscript@users.noreply.github.com>
2022-07-31 18:30:27 +0100

    modal

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -2,3 +2,16 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+
+const mobalBtn = document.querySelector('.modal-btn')
+const mobalOverlay = document.querySelector('.modal-overlay')
+const closeBtn = document.querySelector('.close-btn')
+
+mobalBtn.addEventListener('click', function(){
+    mobalOverlay.classList.add('open-modal')
+})
+
+closeBtn.addEventListener('click', function(){
+    mobalOverlay.classList.remove('open-modal')
+})
